81d867ad08bd11f83fee969094a2660b8b6d856a
LUU HUYNH NGUYEN THI <luuhuynh.tkp@gmail.com>
2023-01-03 13:52:59 +0700

    shoescard

--- src/components/ShoesCard/ShoesCard.jsx
+++ src/components/ShoesCard/ShoesCard.jsx
@@ -1,20 +1,26 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom';
+import styles from './ShoesCard.module.css'
 
 const ShoesCard = (props) => {
 
-  const {prod} = props;
+  const { prod } = props;
   return (
-    <div className='card'>
+    <NavLink to={`/detail/${prod?.id}`}>
+      <div className='card'>
         <img src={prod?.image} alt="..." />
         <div className='card-body'>
-            <h3>{prod?.name}</h3>
-            <p>{prod?.price}</p>
-            <NavLink to={`/detail/${prod?.id}`} className='btn btn-success'>
-                View detail
-            </NavLink>
+          <h3 className={styles.name}>{prod?.name}</h3>
+          <p className={styles.description}>{prod?.shortDescription}</p>
         </div>
-    </div>
+        <div className='d-flex row' style={{ margin: 0, height: '40px' }}>
+          <button className={`${styles.buynow}`}>
+            Buy now</button>
+          <p className={`${styles.price} col-6`}>
+            {prod?.price}$</p>
+        </div>
+      </div>
+    </NavLink >
   )
 }
 
--- src/redux/reducers/userReducer.jsx
+++ src/redux/reducers/userReducer.jsx
@@ -3,19 +3,19 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
-    userLogin: null
+  userLogin: null
 }
 
 const userReducer = createSlice({
   name: 'userReducer',
   initialState,
   reducers: {
-    loginAction: (state,action) => {
-        state.userLogin = action.payload;
+    loginAction: (state, action) => {
+      state.userLogin = action.payload;
     }
   }
 });
 
-export const {loginAction} = userReducer.actions
+export const { loginAction } = userReducer.actions
 
 export default userReducer.reducer
